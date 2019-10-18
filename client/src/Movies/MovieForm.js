24f171998913b3eedb0f632683dbbb335a94bdfb
lowell1 <lowelljacobs@mail.com>
2019-10-17 21:33:46 -0700

    added submit button

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -8,9 +8,11 @@ new input field to add star stars
 
 import React, {useState, useEffect} from "react"
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 const MovieForm = (props) => {
-    const [form, setForm] = useState({title: "", director: "", metascore: "", stars: []});
+    const [form, setForm] = useState({title: "", director: "", metascore: "", newStar: ""});
+    const [stars, setStars] = useState([]);
 
     const handleChange = e => {
         setForm({...form, [e.target.name]: e.target.value});
@@ -19,45 +21,73 @@ const MovieForm = (props) => {
     useEffect(() => {
         axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
         .then(resp => {
-            // setForm({
-            //     title: resp.data.title,
-            //     director: resp.data.director,
-            //     stars: resp.data.stars.map((star, idx) => { return {name: star, id: idx}; })
-            // });
-            setForm(resp.data);
+            console.log(resp)
+            setForm({
+                newStar: "",
+                title: resp.data.title,
+                director: resp.data.director,
+                metascore: resp.data.metascore
+            });
+
+            setStars(resp.data.stars);
         })
         .catch(error => {
             console.log(error);
         })
     },[props.match.params.id]);
 
-    const handleDelete = e => {
+    const deleteStar = e => {
+        const starsCopy = [...stars];
+        starsCopy.splice(e.target.name,1);
+        setStars(starsCopy);
+    }
+
+    const addStar = () => {
+        setStars([...stars, form.newStar]);
+        setForm({...form, newStar: ""});
+    }
+
+    const updateMovie = e => {
+        e.preventDefault();
     }
 
 
     return (
-        <form>
-            <label>
-                Title:
-                <input type="text" onChange={e => handleChange(e)} name="title" value={form.title}/>
-            </label>
+        <div>
+            <form>
+                <label>
+                    Title:
+                    <input type="text" onChange={e => handleChange(e)} name="title" value={form.title}/>
+                </label>
+                <br/>
+                <label>
+                    Director:
+                    <input type="text" onChange={e => handleChange(e)} name="director" value={form.director}/>
+                </label>
+                <br/>
+                <label>
+                    Metascore:
+                    <input type="number" name="metascore" onChange={e => handleChange(e)} value={form.metascore}/>
+                </label>
+    
+                <p>Stars:</p>
+                {
+                    stars.map((star, idx) => 
+                        <p key={idx}>
+                            <button type="button" name={idx} onClick={e => deleteStar(e)}>Delete </button>
+                            {star}
+                        </p>
+                    )
+                }
+                <br/>
+                <button type="button" onClick={() => addStar()}>Add star</button>
+                <input type="text" name="newStar" value={form.newStar} onChange={e => handleChange(e)}/>
+                <br/><br/>
+                <button type="submit" onClick={e => updateMovie(e)}>Update movie</button>
+            </form>
             <br/>
-            <label>
-                Director:
-                <input type="text" onChange={e => handleChange(e)} name="director" value={form.director}/>
-            </label>
-            <br/>
-            <label>
-                Metascore:
-                <input type="number" name="metascore" onChange={e => handleChange(e)} value={form.metascore}/>
-            </label>
-
-            {/* {form.stars.map(star => JSON.stringify(star))} */}
-            {/* <label>
-                Stars:
-                <input type="textarea" name="stars" value={form.stars}/>
-            </label> */}
-        </form>
+            <Link to={`/movies/${props.match.params.id}`}>Go to movie</Link>
+        </div>
     );
 }
 
